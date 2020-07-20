f7f16d185357de3e997a6b31f53ecdbd7fb4bbf9
DiegoLFE <diegolf1000@gmail.com>
2020-07-20 09:25:00 -0300

    correccion

--- 2-InstruccionIf/jsInstruccionIF-08.js
+++ 2-InstruccionIf/jsInstruccionIF-08.js
@@ -4,8 +4,11 @@ function mostrar()
 	var estadoCivil;
 		 edad=document.getElementById("txtIdEdad").value;
 		 estadoCivil=document.getElementById("estadoCivil").value;
-		 if (edad<18&&estadoCivil!="Soltero"){
-		}else {
+		 if (edad<18&&estadoCivil!="Soltero")	
+		 {
+		//NO HACER NADA
+		 }	
+		 else {
 			if (edad>=18&&estadoCivil=="Soltero"){
 				alert("Es soltero y no es menor.")
 			}
@@ -13,4 +16,5 @@ function mostrar()
 		}
 }
 /*Al ingresar una edad menor a 18 años y un estado civil distinto a "Soltero", NO HACER NADA,
-pero si no es asi, y es soltero y no es menor, mostrar el siguiente mensaje: 'Es soltero y no es menor.'*/
+pero si no es asi, y es soltero y no es menor, mostrar el siguiente mensaje: 'Es soltero y no es menor.'
+*/
--- 2-InstruccionIf/jsInstruccionIF-09.js
+++ 2-InstruccionIf/jsInstruccionIF-09.js
@@ -1,11 +1,11 @@
 function mostrar()
 {	
-	getElementsByClassName("MiBotonUTNnav").onclick= function() {miFuncion()};
+	//getElementsByClassName("MiBotonUTNnav").onclick= function() {miFuncion()};
 
-	function miFuncion() {
-		document.getElementById("demo").innerHTML = Math.random(0,1,2,3,4,5,6,7,8,9,10);
+	//function miFuncion() {
+	//	document.getElementById("demo").innerHTML = Math.random(0,1,2,3,4,5,6,7,8,9,10);
 
-	  }
+	 // }
 
 }
 /*Al presionar el Botón, mostrar un número Random del 1 al 10 inclusive 
